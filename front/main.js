711f4b52cf774a6644eaa03217a04ba424ec1cc1
ImanHpr <imanhpr1999@gmail.com>
2022-05-07 09:45:22 +0430

    implement sendData in mainapp

--- front/main.js
+++ front/main.js
@@ -1,7 +1,15 @@
-import { alertMaker, listItemMaker } from "./maker.js";
+import { alertMaker, listItemMaker, sendData } from "./maker.js";
 
 const btnd = document.getElementById("main-btn");
 const textbox = document.getElementById('text-box');
 btnd.addEventListener('click', () => {
-    textbox.value.trim() !== '' ? listItemMaker(textbox.value) : alertMaker();
+    if (textbox.value.trim() !== '') {
+        listItemMaker(textbox.value);
+        (async() => {
+            const response = await sendData(textbox.value.trim());
+            console.log(response)
+        })();
+    } else {
+        alertMaker('We can\'t submit empty textbox');
+    }
 })
